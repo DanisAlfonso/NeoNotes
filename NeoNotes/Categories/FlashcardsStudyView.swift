c591f40fbcd34cf5b13afdbb347c524fa74ee256
Danis <danis.ramirez.hn@gmail.com>
2023-11-23 19:25:17 +0100

    Implementing FSRS engine

--- NeoNotes/Categories/FlashcardsStudyView.swift
+++ NeoNotes/Categories/FlashcardsStudyView.swift
@@ -6,64 +6,99 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct FlashcardsStudyView: View {
-    @ObservedObject var category: Category
-    @State private var currentFlashcardIndex = 0
-    @State private var showAnswer = false
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var currentFlashcard: Flashcard?
+    var category: Category
+
+    let fsrsEngine = FSRS()
 
     var body: some View {
         VStack {
-            let flashcards = category.flashcardsArray
-            if flashcards.count > 0 {
-                let flashcard = flashcards[currentFlashcardIndex]
-
-                VStack {
-                    Text(flashcard.question ?? "No content")
-                        .font(.title)
-                        .padding()
-
-                    Button("Show Answer") {
-                        showAnswer.toggle()
-                    }
-
-                    if showAnswer {
-                        Text(flashcard.answer ?? "No answer")
-                            .font(.body)
-                            .padding()
-                    }
-                }
-                .padding()
+            if let flashcard = currentFlashcard {
+                Text(flashcard.question ?? "No Question")
 
                 HStack {
-                    Button("I didn't know") {
-                        // Logic for incorrect answer
-                        moveToNextFlashcard()
-                    }
-                    .padding()
-
-                    Button("I knew it") {
-                        // Logic for correct answer
-                        moveToNextFlashcard()
-                    }
-                    .padding()
+                    Button("Again") { rateFlashcard(rating: .Again) }
+                    Button("Hard")  { rateFlashcard(rating: .Hard)  }
+                    Button("Good")  { rateFlashcard(rating: .Good)  }
+                    Button("Easy")  { rateFlashcard(rating: .Easy)  }
                 }
             } else {
-                Text("No flashcards in this category.")
+                Text("No flashcards to review.")
             }
         }
-        .navigationTitle("Studying: \(category.name ?? "Unknown")")
+        .onAppear {
+            loadNextFlashcard(from: category)
+        }
     }
 
-    private func moveToNextFlashcard() {
-        showAnswer = false
-        let flashcards = category.flashcardsArray
-        if currentFlashcardIndex < flashcards.count - 1 {
-            currentFlashcardIndex += 1
+    private func loadNextFlashcard(from category: Category) {
+        let currentDate = Date()
+
+        // Fetch the next due flashcard
+        if let nextFlashcard = category.flashcardsArray.first(where: { flashcard in
+            // Assuming 'due' is a Date and flashcards are due when 'due' <= currentDate
+            // This logic might change based on your app's specific requirements
+            (flashcard.due ?? currentDate) <= currentDate
+        }) {
+            self.currentFlashcard = nextFlashcard
         } else {
-            // Reset or finish the study session
-            currentFlashcardIndex = 0
+            self.currentFlashcard = nil
         }
+    }
+
+    private func rateFlashcard(rating: Rating) {
+        guard let flashcard = currentFlashcard else { return }
+        let card = flashcard.toCard()
+
+        // Use FSRS engine to update the card based on the rating
+        let updatedCardInfo = fsrsEngine.repeat(card: card, now: Date())
+
+        // Choose the updated card based on the rating
+        if let updatedCard = updatedCardInfo[rating]?.card {
+            flashcard.updateFromCard(updatedCard)
+            saveContext()
+
+            loadNextFlashcard(from: category)
+        }
+    }
+
+    private func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            // Handle the error
+        }
+    }
+}
+
+
+extension Flashcard {
+    func toCard() -> Card {
+        let card = Card()
+        // Map Flashcard properties to Card properties
+        card.due = self.due ?? Date() // Provide a default value if `self.due` is nil
+        card.difficulty = self.difficulty
+        card.stability = self.stability
+        card.reps = Int(self.reps)
+        card.lapses = Int(self.lapses)
+        card.status = Status(rawValue: Int(self.status)) ?? .New
+        card.lastReview = self.lastReview ?? Date()
+        return card
+    }
+
+    func updateFromCard(_ card: Card) {
+        // Map Card properties back to Flashcard
+        self.due = card.due // 'card.due' is non-optional, so direct assignment is fine
+        self.difficulty = card.difficulty
+        self.stability = card.stability
+        self.reps = Int16(card.reps)
+        self.lapses = Int16(card.lapses)
+        self.status = Int16(card.status.rawValue)
+        self.lastReview = card.lastReview
     }
 }
 
@@ -73,3 +108,27 @@ extension Category {
         return set.sorted { $0.creationDate ?? Date() < $1.creationDate ?? Date() }
     }
 }
+
+struct FlashcardsStudyView_Previews: PreviewProvider {
+    static var previews: some View {
+        let context = PersistenceController(inMemory: true).container.viewContext
+        let newCategory = Category(context: context)
+        newCategory.name = "Sample Category"
+
+        // Add some sample flashcards to the category
+        let flashcard1 = Flashcard(context: context)
+        flashcard1.question = "1 + 1"
+        flashcard1.answer = "2"
+        flashcard1.category = newCategory
+
+        let flashcard2 = Flashcard(context: context)
+        flashcard2.question = "2 + 2"
+        flashcard2.answer = "4"
+        flashcard2.category = newCategory
+
+        return FlashcardsStudyView(category: newCategory)
+            .environment(\.managedObjectContext, context)
+            .frame(width: 400, height: 400)
+    }
+}
+
--- NeoNotes/NeoNotesApp.swift
+++ NeoNotes/NeoNotesApp.swift
@@ -25,12 +25,38 @@ struct PersistenceController {
 
     let container: NSPersistentContainer
 
-    init() {
+    init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "NeoNotesModel")
+
+        // Set the options for the persistent store directly
+        if let description = container.persistentStoreDescriptions.first {
+            description.setOption(NSNumber(value: true), forKey: NSMigratePersistentStoresAutomaticallyOption)
+            description.setOption(NSNumber(value: true), forKey: NSInferMappingModelAutomaticallyOption)
+        }
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+
         container.loadPersistentStores { (storeDescription, error) in
             if let error = error as NSError? {
+                // Handle the error appropriately
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         }
     }
 }
+
+extension PersistenceController {
+    static var preview: PersistenceController = {
+        let controller = PersistenceController(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        // Create mock data here if needed
+
+        return controller
+    }()
+}
+
+
+
+
